70b3811993cf16fa2b2edc506b5362118451a7d6
Jose Kho <jakho@Jose-Khos-MacBook-Pro.local>
2014-09-17 20:26:22 -1000

    added a test class

---
+++ Git Test/JKTest.h
@@ -0,0 +1,13 @@
+//
+//  JKTest.h
+//  Git Test
+//
+//  Created by Jose Kho on 9/17/14.
+//  Copyright (c) 2014 Jag Kho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JKTest : NSObject
+
+@end
---
+++ Git Test/JKTest.m
@@ -0,0 +1,13 @@
+//
+//  JKTest.m
+//  Git Test
+//
+//  Created by Jose Kho on 9/17/14.
+//  Copyright (c) 2014 Jag Kho. All rights reserved.
+//
+
+#import "JKTest.h"
+
+@implementation JKTest
+
+@end
